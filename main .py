2bf1472b3c6e086a1f7d2260782293245dfe27e2
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-25 10:03:24 +0330

    Add numbers & all.

--- main .py
+++ main .py
@@ -2,5 +2,7 @@ import random
 import string
 Lower=string. ascii_Lowercase
 Upper=string.ascii_Uppercase
-symbols"!#@$%^&?:{}[]"
+symbols="!#@$%^&?:{}[]"
+numbers="0123456789"
+all=Lower+Upper+symbols+numbers
 
