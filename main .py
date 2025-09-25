60372095daa965c00f890bfe774dcccc5e289a26
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-25 10:27:13 +0330

    Add if condition.

--- main .py
+++ main .py
@@ -8,5 +8,12 @@ all=Lower + Upper + symbols + numbers
 while(True):
     print("choose an option:\n\t 1)Creat a password\n\t 2)Exit")
     choice = input("enter your choice:")
+    if choice == "1":
+        Length = int(input("Enter the length of password:"))
+        password = "".join((random.sample(all, Length)))
+        print(password)
+        
+    
+    
 
 
